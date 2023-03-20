a4839b81e308f847371639a4ec17d73bd9f70776
Jo Wagner <jowagner@limago.de>
2023-03-20 11:23:57 +0100

    Uebung

--- Tag1_01Stapel/.idea/sonarlint/securityhotspotstore/index.pb
+++ Tag1_01Stapel/.idea/sonarlint/securityhotspotstore/index.pb
@@ -7,3 +7,11 @@ V
 &src/test/java/de/atruvia/TestTest.java,3\5\358dfbcae8d7ec6d1033a2258293bbc02a1ec819
 i
 9src/main/java/de/atruvia/collections/StapelException.java,e\0\e0da989592e4baeaaed4afcb2c14d2f6b396a076
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8\8\88a9255124c95bdc913197c120a8d560edc59c8e
+d
+4src/test/java/de/atruvia/collections/StapelTest.java,d\4\d4476cb5a9ec9248c6669ba8167a4e0efa753fda
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
+`
+0src/main/java/de/atruvia/collections/Stapel.java,e\b\eb7f5157b4ef8a1afa2bd2ebcd22f446cd5516cb
--- Tag1_02Schwein/src/main/java/de/atruvia/tiere/Schwein.java
+++ Tag1_02Schwein/src/main/java/de/atruvia/tiere/Schwein.java
@@ -1,10 +1,17 @@
 package de.atruvia.tiere;
 
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+
 import java.util.Objects;
 
+
+@Data
 public class Schwein {
 
     private String name;
+    @Setter(AccessLevel.PRIVATE)
     private int gewicht;
 
     public Schwein() {
@@ -12,50 +19,22 @@ public class Schwein {
     }
 
     public Schwein(String name) {
-        this.name = name;
-        this.gewicht = 10;
+        setName(name);
+        setGewicht(10);
     }
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
+
+    public final void setName(String name) {
         if("elsa".equalsIgnoreCase(name)) throw new IllegalArgumentException("Häh");
         this.name = name;
     }
 
-    public int getGewicht() {
-        return gewicht;
-    }
 
-    private void setGewicht(int gewicht) {
-        this.gewicht = gewicht;
-    }
 
     public void fuettern() {
         setGewicht(getGewicht() + 1);
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Schwein schwein = (Schwein) o;
-        return gewicht == schwein.gewicht && Objects.equals(name, schwein.name);
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, gewicht);
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("Schwein{");
-        sb.append("name='").append(name).append('\'');
-        sb.append(", gewicht=").append(gewicht);
-        sb.append('}');
-        return sb.toString();
-    }
 }
