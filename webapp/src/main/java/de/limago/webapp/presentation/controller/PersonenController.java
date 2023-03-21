48eb668e4ddbc57f01d6eb6a54eeec94175f860a
Jo Wagner <jowagner@limago.de>
2023-03-21 15:37:45 +0100

    Controller Test

--- webapp/src/main/java/de/limago/webapp/presentation/controller/PersonenController.java
+++ webapp/src/main/java/de/limago/webapp/presentation/controller/PersonenController.java
@@ -5,14 +5,15 @@ import de.limago.webapp.presentation.dto.PersonDTO;
 import de.limago.webapp.presentation.mapper.PersonDtoMapper;
 import de.limago.webapp.service.PersonenService;
 import de.limago.webapp.service.PersonenServiceException;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/v1/personen")
@@ -26,4 +27,31 @@ public class PersonenController {
     public ResponseEntity<PersonDTO> findPersonById(@PathVariable  String id) throws PersonenServiceException {
         return ResponseEntity.of(service.findeNachId(id).map(mapper::convert));
     }
+
+
+    @GetMapping(path="", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Iterable<PersonDTO>> findAll(
+            @RequestParam(required = false, defaultValue = "") String vorname,
+            @RequestParam(required = false, defaultValue = "") String nachname
+    ) throws PersonenServiceException {
+
+        return ResponseEntity.ok(mapper.convert(service.findeAlle()));
+    }
+
+    @PostMapping(path = "", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> save(@Valid @RequestBody  PersonDTO person, UriComponentsBuilder builder ) throws Exception{
+
+        service.speichern(mapper.convert(person));
+        var uri = builder.path("/v1/personen/{id}").buildAndExpand(person.getId());
+        return ResponseEntity.created(uri.toUri()).build();
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> remove(@PathVariable String id ) throws Exception{
+
+        if(service.loesche(id))
+                return ResponseEntity.ok().build();
+        return ResponseEntity.notFound().build();
+    }
+
 }
--- webapp/src/test/java/de/limago/webapp/presentation/controller/PersonenControllerTest.java
+++ webapp/src/test/java/de/limago/webapp/presentation/controller/PersonenControllerTest.java
@@ -1,14 +1,21 @@
 package de.limago.webapp.presentation.controller;
 
 import de.limago.webapp.presentation.dto.PersonDTO;
+import de.limago.webapp.repository.PersonenRepository;
 import de.limago.webapp.service.PersonenService;
+import de.limago.webapp.service.PersonenServiceException;
 import de.limago.webapp.service.model.Person;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
@@ -16,8 +23,12 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
+
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -32,6 +43,9 @@ class PersonenControllerTest {
 
     @MockBean
     private PersonenService serviceMock;
+
+//    @MockBean
+//    private PersonenRepository repoMock;
 
     private final String validUUID = "b2e24e74-8686-43ea-baff-d9396b4202e0";
     private final PersonDTO validPersonDTO = PersonDTO.builder().id(validUUID).vorname("John").nachname("Doe").build();
@@ -66,6 +80,75 @@ class PersonenControllerTest {
         ResponseEntity<PersonDTO> entity = restTemplate.getForEntity("/v1/personen/b2e24e74-8686-43ea-baff-d9396b4202e0", PersonDTO.class);
 
         assertEquals(HttpStatus.NOT_FOUND, entity.getStatusCode());
+    }
+
+    @Test
+    void findAllo_test4() throws Exception{
+        Iterable<Person> personen = List.of(Person.builder().id("1").vorname("John").nachname("Doe").build(),Person.builder().id("2").vorname("John").nachname("Rambo").build());
+
+        when(serviceMock.findeAlle()).thenReturn(personen);
+        ResponseEntity<List<PersonDTO>> entity = restTemplate.exchange("/v1/personen", HttpMethod.GET, null,new ParameterizedTypeReference<List<PersonDTO>>() { });
+
+        assertEquals(HttpStatus.OK, entity.getStatusCode());
+        List<PersonDTO> liste = entity.getBody();
+//        assertThat(liste)
+//                .isNotEmpty()
+//                .contains("1")
+//                .doesNotContainNull()
+//                .containsSequence("2", "3");
+        assertEquals(2,liste.size());
+        for(var element : liste) {
+            assertEquals("John",element.getVorname());
+        }
+    }
+
+    @Test
+    void save_HappyDay() throws Exception{
+
+        PersonDTO dtoToSend =PersonDTO.builder().id(validUUID).vorname("John").nachname("Doe").build();
+
+        HttpEntity<PersonDTO> httpEntity = new HttpEntity<>(dtoToSend);
+
+        ResponseEntity<Void> entity = restTemplate.exchange("/v1/personen", HttpMethod.POST, httpEntity,Void.class);
+
+        assertEquals(HttpStatus.CREATED, entity.getStatusCode());
+        assertThat(entity.getHeaders().getLocation().toString(), Matchers.endsWith("/v1/personen/b2e24e74-8686-43ea-baff-d9396b4202e0"));
+        verify(serviceMock).speichern(validPerson);
+    }
+
+    @Test
+    void save_Antipath() throws Exception{
+
+
+        doThrow(new PersonenServiceException("Unerwuenschte Person")).when(serviceMock).speichern(any());
+
+        PersonDTO dtoToSend =PersonDTO.builder().id(validUUID).vorname("John").nachname("Doe").build();
+
+        HttpEntity<PersonDTO> httpEntity = new HttpEntity<>(dtoToSend);
+
+        ResponseEntity<Map<String, Object>> entity = restTemplate.exchange("/v1/personen", HttpMethod.POST, httpEntity,new ParameterizedTypeReference<Map<String, Object>>() { });
+
+        assertEquals(HttpStatus.BAD_REQUEST, entity.getStatusCode());
+        var result = entity.getBody();
+        assertEquals("Unerwuenschte Person", result.get("message"));
+
+    }
+    @Test
+    void save_InternerServerError() throws Exception{
+
+
+        doThrow(new PersonenServiceException("Interner Fehler")).when(serviceMock).speichern(any());
+
+        PersonDTO dtoToSend =PersonDTO.builder().id(validUUID).vorname("John").nachname("Doe").build();
+
+        HttpEntity<PersonDTO> httpEntity = new HttpEntity<>(dtoToSend);
+
+        ResponseEntity<Map<String, Object>> entity = restTemplate.exchange("/v1/personen", HttpMethod.POST, httpEntity,new ParameterizedTypeReference<Map<String, Object>>() { });
+
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, entity.getStatusCode());
+        var result = entity.getBody();
+        assertEquals("Interner Fehler", result.get("message"));
+
     }
 
 }
